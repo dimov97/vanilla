6a51b8451e2262b5412e54c2d16b6ebf39eb4ece
Ihor <dimov.igor97@gmail.com>
2022-09-06 21:01:43 +0300

    tests

---
+++ my-app/src/01/01_01.ts
@@ -0,0 +1,19 @@
+export const value01_01 = '01_01'
+
+export function sum(a: number, b: number) {
+    return a + b
+}
+
+export function mult(a: number, b: number) {
+    return a * b
+}
+
+export function splitIntoWords(sentence: string) {
+    return sentence.toLowerCase().split(' ')
+        .filter(w => w !== '' && w !== '-')
+        .map(w => w
+            .replace('!', '')
+            .replace('.', '')
+            .replace(',', '')
+        )
+}
